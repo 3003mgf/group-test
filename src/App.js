08cae256550028b4381ea06388dd7fdfd01b557b
Ignacio Gramajo Feijoo <118638937+3003mgf@users.noreply.github.com>
2023-07-09 19:14:22 -0300

    rfr

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hello world! From test branch.</h2>
+      <h2>Hello world! From test branch. TEST2</h2>
     </div>
   );
 }
